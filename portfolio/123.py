d5f1788127813da84cf738f201fdd1dc884ba44d
achisum94 <34990703+achisum94@users.noreply.github.com>
2018-10-26 20:44:06 -0400

    Delete 123.py

--- portfolio/123.py
+++
@@ -1,7 +0,0 @@
-discount_rate=50/100
-original=int(input('what is the original price for the product '))
-sales_price=original*discount_rate
-
-print ('the price after 50% discount is $', sales_price,sep='')
-
-
